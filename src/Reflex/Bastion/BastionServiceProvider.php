42fdab0cdc296d9d051138a2225a5d1acd5f3dc8
Azirius <me@aziri.us>
2013-07-07 13:41:23 +0100

    added provider

---
+++ src/Reflex/Bastion/BastionServiceProvider.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Bastion
+ *
+ * @package   Reflex
+ * @version   1.0.0
+ * @author    Reflex Community
+ * @license   MIT License
+ * @copyright 2013 Reflex Community
+ * @link      http://reflex.aziri.us/
+ */
+
+namespace Reflex\Bastion;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * BastionServiceProvider
+ *
+ * @package    Reflex
+ * @subpackage Core
+ */
+class BastionServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->package('reflex/bastion');
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array();
+    }
+}
